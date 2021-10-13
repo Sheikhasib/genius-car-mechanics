5af647e81ba43b0ee58bbc3be61a93e119652b91
Sheikhasib <sheikhhasib037@gmail.com>
2021-10-13 17:21:21 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+// import banner1 from '../../../images/banner/banner-1.jpg';
+// import banner2 from '../../../images/banner/banner-2.jpg';
+// import banner3 from '../../../images/banner/banner-3.jpg';
+
+
+const Banner = () => {
+  return (
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/3hzRNk2/Banner-1.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/hHRbzJY/Banner-2.jpg"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/wpmys11/Banner-3.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
+
+export default Banner;
---
+++ src/Pages/Home/Experts/Experts.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Expert from '../Expert/Expert';
+
+
+const experts = [
+    {
+        img:'https://i.ibb.co/gZPV5zc/Car-joy-smiling-spanner-occupation.jpg', name:'Andrew Smith', 
+        expertize:'-Engine Experts'
+    },
+    {
+        img:'https://i.ibb.co/mRGFFrX/Bearded-man-changing-tire-his-car.jpg', 
+        name:'John Anderson', 
+        expertize:'-Polish Experts'
+    },
+    {
+        img:'https://i.ibb.co/L9PLw4c/Mechanic-changing-tires-car-service.jpg', name:'Zuku Mama', 
+        expertize:'-Coloring Experts'
+    },
+    {
+        img:'https://i.ibb.co/XVKRkCH/Smiling-mechanic-with-arms-crossed-spanner.jpg', name:'Kalia Mutha', 
+        expertize:'-Mutha Experts'
+    },
+    {
+        img:'https://i.ibb.co/60YdPxN/Hand-car-mechanic-with-wrench-auto-repair-garage.jpg', 
+        name:'Gulli Boy', 
+        expertize:'-Mouth Experts'
+    }
+]
+
+const Experts = () => {
+    return (
+        <div className="container">
+            <h2 className="text-primary mt-5">Our Experts</h2>
+            <div className="row">
+                {
+                    experts.map(expert => <Expert 
+                        key={expert.name}
+                        expert={expert}
+                    ></Expert>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Experts;
